1b4ed8e9a48f41c2c7872df01fa1894a4d6c8a4d
Louis Aslett <louis@louisaslett.com>
2023-03-14 11:44:16 +0000

    Tweak fuseki docker image to ghcr package

--- fuseki/_build.sh
+++ fuseki/_build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-docker build . -t ghcr.io/louisaslett/fairvasc-federated/fuseki:latest
+docker build . -t fairvasc_fuseki
+docker tag fairvasc_fuseki ghcr.io/louisaslett/fairvasc_fuseki:latest
 
 echo "PAT_HERE" | docker login ghcr.io -u louisaslett --password-stdin
 
-docker push ghcr.io/louisaslett/fairvasc-federated/fuseki:latest
+docker push ghcr.io/louisaslett/fairvasc_fuseki:latest
